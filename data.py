81a84a4e4c3c1691c255e3cbddea28d6620488aa
bbq12340 <58031020+bbq12340@users.noreply.github.com>
2020-09-20 19:05:32 +0900

    4th commit

--- data.py
+++ data.py
@@ -1,3 +1,5 @@
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
 import pandas as pd
 
 from scraper import Scraper
@@ -7,15 +9,21 @@ class Reader:
         self.filename = filename
         self.limit = limit
         self.read_file()
+        self.browser = self.open_browser()
         self.extract_file()
+        self.browser.quit()
 
     def read_file(self):
         request_df = pd.read_csv(self.filename, names=['names', 'link'], sep='*')
         request_df = request_df.set_index('names')
         request_df.index = request_df.index + request_df.groupby(level=0).cumcount().astype(str).replace('0','')
         request_df.to_csv('src/output/wd.csv', encoding='utf-8', header=False)
+        
+    def open_browser(self):
+        browser = webdriver.Chrome(ChromeDriverManager().install())
+        return browser
 
     def extract_file(self):
         df = pd.read_csv('src/output/wd.csv', encoding='utf-8', names=['names', 'link'])
         for i in range(len(df.index)):
-            Scraper(list(df['names'])[i], list(df['link'])[i], self.limit)
+            Scraper(self.browser, list(df['names'])[i], list(df['link'])[i], self.limit)
