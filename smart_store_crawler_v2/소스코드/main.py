cf9ff7f106631790ae715aa37aee97e748607c92
David Jang <bbq12340@hotmail.com>
2020-09-24 21:38:58 +0900

    version 2.0

---
+++ smart_store_crawler_v2/소스코드/main.py
@@ -0,0 +1,65 @@
+import tkinter as tk
+from tkinter import messagebox,filedialog
+
+from data import Reader
+
+root = tk.Tk()
+root.title('스토어 리뷰 크롤러')
+root.geometry('250x150')
+
+#var
+limit = tk.IntVar()
+no_limit = tk.IntVar()
+delay_pages = tk.IntVar()
+delay_time = tk.DoubleVar()
+FILENAME = []
+
+#command
+def add_file():
+    filename = filedialog.askopenfilename(initialdir='/', title='파일 탐색', filetypes=[("text files", "*.txt")])
+    filename_label = tk.Label(root, text=f"{filename.split('/')[-1]}")
+    filename_label.grid(row=0, column=1)
+    FILENAME.append(filename)
+
+def start_process():
+    if no_limit.get() == 0:
+        Reader(FILENAME[-1], limit=limit.get(), delay_pages=delay_pages.get(), delay_time=delay_time.get())
+        messagebox.showinfo('info', '크롤링 완료!')
+    else:
+        Reader(FILENAME[-1], delay_pages=delay_pages.get(), delay_time=delay_time.get())
+        messagebox.showinfo('info', '크롤링 완료!')
+
+#label
+request_label = tk.Label(root, text='요청 파일명:')
+request_label.grid(row=0, column=0)
+
+limit_label = tk.Label(root, text='최소 수량')
+limit_label.grid(row=1, column=0)
+
+delay_item_label = tk.Label(root, text="딜레이 페이지")
+delay_item_label.grid(row=2, column=0)
+
+delay_time_label = tk.Label(root, text="딜레이 (초)")
+delay_time_label.grid(row=3, column=0)
+
+#checkbutton
+limit_check = tk.Checkbutton(root, text='전체', variable=no_limit)
+limit_check.grid(row=1, column=2)
+
+#entry
+limit_entry = tk.Entry(root, width=7, textvariable=limit)
+limit_entry.grid(row=1, column=1)
+
+delay_item_entry = tk.Entry(root, width=7, textvariable=delay_pages)
+delay_item_entry.grid(row=2, column=1)
+
+delay_time_entry = tk.Entry(root, width=7, textvariable=delay_time)
+delay_time_entry.grid(row=3, column=1)
+
+#button
+search_btn = tk.Button(root, text='파일 탐색', command=add_file)
+search_btn.grid(row=0, column=2)
+start_btn = tk.Button(root, text='시작', command=start_process)
+start_btn.grid(row=3, column=2)
+
+root.mainloop()
